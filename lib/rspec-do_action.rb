fb616d74cd8d4599f203b32e442f2f6719d84529
sunteya <sunteya@gmail.com>
2014-08-18 15:55:46 +0800

    fix hack missing on rspec3

--- lib/rspec-do_action.rb
+++ lib/rspec-do_action.rb
@@ -48,12 +48,12 @@ module Rspec
 end
 
 class RSpec::Core::Example
-  def run_before_each_with_action
-    run_before_each_without_action
+  def run_before_example_with_action
+    run_before_example_without_action
     example_group_instance.send(:auto_do_action_once)
   end
-  alias_method :run_before_each_without_action, :run_before_each
-  alias_method :run_before_each, :run_before_each_with_action
+  alias_method :run_before_example_without_action, :run_before_example
+  alias_method :run_before_example, :run_before_example_with_action
 end
 
 RSpec.configure do |config|
